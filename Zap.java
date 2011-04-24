5b8418cfd6d9eb9b827579ab1c04e9a70b3a243b
Oracular <Mr.Mudkipz@gmail.com>
2011-04-23 18:06:35 -0700

    Lightning control

--- Zap.java
+++ Zap.java
@@ -1,10 +1,9 @@
-
 package me.oracular.Zap;
 
 import java.util.List;
-
 import java.util.logging.Logger;
 
+import me.oracular.Zap.commands.ZapCommand;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -21,6 +20,7 @@ public class Zap extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		log.info("Zap disabled");
+		getCommand("zap").setExecutor((new ZapCommand(this)));
 		
 	}
 
@@ -30,89 +30,4 @@ public class Zap extends JavaPlugin {
 		
 	}
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
-	{
-		List<World> worlds = this.getServer().getWorlds();
-		int counter = 0;
-		for (World world : worlds)
-		{
-			if (isParsableToInt(args[0]) == true)
-			{
-				List<Entity> entities = world.getEntities();
-				for (Entity entity : entities)
-				{
-					if (entity.getEntityId() == Integer.parseInt(args[0]))
-					{
-						while (counter < Integer.parseInt(args[1]))
-						{
-							world.strikeLightning(entity.getLocation());
-							counter++;
-							try {
-								Thread.sleep(10L);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-			}
-			else
-			{
-				if (args[0].equalsIgnoreCase("^"))
-				{
-					while (counter < Integer.parseInt(args[1]))
-					{
-						Player stuff = (Player) sender;
-						world.strikeLightning(stuff.getTargetBlock(null,256).getLocation());
-						counter++;
-						try {
-							Thread.sleep(10L);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-				else
-				{
-					Player player = getPlayer(args[0]);
-					while (counter < Integer.parseInt(args[1]))
-					{
-						world.strikeLightning(player.getLocation());
-						counter++;
-						try {
-							Thread.sleep(10L);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-		return true;
-	}
-
-	public Player getPlayer(String player) {
-		List<Player> players = this.getServer().matchPlayer(player);
-		if (players.isEmpty()) {
-			return null;
-		} else {
-			return players.get(0);
-		}
-	}
-	
-	public boolean isParsableToInt(String i)
-	{
-		try
-		{
-			Integer.parseInt(i);
-			return true;
-		}
-		catch(NumberFormatException nfe)
-		{
-			return false;
-		}
-	}
 }
